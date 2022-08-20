81d596ceddbe561830e726a86565dad96877c0d0
vveoneix <88292705+vveoneix@users.noreply.github.com>
2022-08-20 23:59:54 +0200

    themes for website with no content

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,4 +1,5 @@
 let mobile = window.matchMedia("(max-width: 768px)")
+let root = document.querySelector(":root");
 const tiles = ["tile-butters", "tile-mystery", "tile-github"]
 
 function getRandInt(min, max){
@@ -75,4 +76,32 @@ function playAnimation(element){
 
 function stopAnimation(element){
     document.getElementById(element).classList.add("pauseanimation");
+}
+
+function changeTheme(theme) {
+    let bg = Math.floor(Math.random() * themes[theme][4] ) + 1;
+    let background = "url(images/" + themes[theme][0] + "/" + themes[theme][0] + bg + ".png)";
+    let body = document.querySelector("body");
+    
+    body.classList.remove('fade-in');
+    void body.offsetWidth;
+    body.classList.add('fade-in');
+
+    root.style.setProperty('--accent1', themes[theme][1]);
+    root.style.setProperty('--accent2', themes[theme][2]);
+    root.style.setProperty('--bgdark', themes[theme][3]);
+    root.style.setProperty('--welcomebg', background);
+    root.style.setProperty('--theme', theme);
+    document.querySelectorAll('.thing').forEach(element => {
+        element.innerHTML = themes[theme][0];
+    });
+}
+
+function randomTheme() {
+    let newTheme;
+    do {
+        newTheme = getRandInt(0,themes.length)
+    }
+    while(newTheme == getComputedStyle(root).getPropertyValue('--theme'))
+    changeTheme(newTheme);
 }
